d17b798810059f690bf13dbcefc2ad6a595b5e78
Farangis <farangis98@ya.ru>
2023-08-21 14:44:22 +0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Farangis on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
