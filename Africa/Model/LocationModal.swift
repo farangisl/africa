77d554e9b0959e68e991b4da01cd5bc745932ab2
Farangis <farangis98@ya.ru>
2023-08-23 17:01:25 +0500

    gallery

---
+++ Africa/Model/LocationModal.swift
@@ -0,0 +1,21 @@
+//
+//  LocationModal.swift
+//  Africa
+//
+//  Created by Farangis on 23/08/23.
+//
+
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
